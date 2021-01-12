bf62f8717fc919dea8c4c019ade2da91e96c8cd6
Steven Basher <sbasher314@gmail.com>
2021-01-12 18:45:22 -0500

    Complete Grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,8 @@
 var Grub = function() {
+  //need a grub class with age set 0, color, and food properties and an eat method
+  this.age = 0;
+  this.color = 'pink';
+  this. food = 'jelly';
 };
 
+Grub.prototype.eat = function() {};
